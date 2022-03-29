6df1560a271b316926edeb4133a396bb04f6e379
Pupa5E <hkhunk0202@icloud.com>
2022-03-30 00:42:54 +0900

    this is simple server

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require('express')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
-const dotenv = require('cotenv')
+const dotenv = require('dotenv')
 const path = require('path')
 
 dotenv.config()
+const indexRouter = require('./routes')
+const userRouter = require('./routes/user')
 const app = express()
 app.set('port',process.env.PORT||3000)
 
 app.use(morgan('dev'))
-app.use('/',express.static(path.join(__dirname),'public'))
+app.use('/',express.static(path.join(__dirname,'public')))
 app.use(express.json())
-app.use(express,urlencoded({extends:false}))
+app.use(express.urlencoded({extends:false}))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(session({
     resave: false,
@@ -24,16 +26,14 @@ app.use(session({
     },
     name: 'session-cookie',
 }))
-app.use((req, rse, next)=>{
-    next()
+
+app.use('/',indexRouter)
+app.use('/user',userRouter)
+
+app.use((req,res,next)=>{
+    res.status(404).send('Not Found')
 })
 
-app.get('/',(req, res, next)=>{
-    res.send('hello world')
-    next()
-}, (req,res)=>{
-    throw new Error('error')
-})
 app.use((err,req,res,next)=>{
     console.log(err)
     res.status(500).send(err.message)
